4ab98eb6195d7b3cd278eccd93d8d46fcffea3f8
Aleks Pashkevych <alekspashkevych@gmail.com>
2025-09-24 19:04:11 +0300

    some test are failed

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,23 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const cssParams = sourceString.split(';');
+  const cssValues = {};
+
+  for (let i = 0; i < cssParams.length; i++) {
+    if (cssParams[i].trim().length === 0) {
+      break;
+    }
+
+    const arr = cssParams[i].split(':');
+    const key = arr[0].trim();
+    const value = arr[1].trim();
+
+    cssValues[key] = value;
+  }
+
+  // console.log(cssValues);
+  return cssValues;
 }
 
 module.exports = convertToObject;
